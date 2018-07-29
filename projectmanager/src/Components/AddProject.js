b990b00e4c3717a0303d2f0fdede1934f77928cc
tpham20908 <tpham20908@gmail.com>
2018-07-29 12:03:50 -0400

    bound handleSubmit of AddProject to handleAddProject of main App

--- projectmanager/src/Components/AddProject.js
+++ projectmanager/src/Components/AddProject.js
@@ -14,20 +14,30 @@ class AddProject extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log("Submitted!");
+        if (this.refs.title.value === "") {
+            alert("Title is required!");
+        } else {
+            this.setState({newProject: {
+                title: this.refs.title.value,
+                category: this.refs.category.value
+            }}, function() {
+                // console.log(this.state.newProject);
+                this.props.addProject(this.state.newProject);
+            });
+        }
     }
 
     render() {
         let categoryOptions = this.props.categories.map(category => {
             return(
-                <option key = {category} value = "category" >{category}</option>
+                <option key = {category} value = {category} >{category}</option>
             );
         });
 
         return (
             <div>
                 <h3>Add Project</h3>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div>
                         <label>Title</label><br/>
                         <input type="text" ref="title" />
