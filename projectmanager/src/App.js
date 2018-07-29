a8918c734f4d2609921311ab6d103f8014b22be7
tpham20908 <tpham20908@gmail.com>
2018-07-28 22:56:53 -0400

    create state in App class constructor

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -3,6 +3,24 @@ import Projects from "./Components/Projects";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    this.state = {
+      projects = [
+        {
+          title: "Business Website",
+          category: "Web Design"
+        },
+        {
+          title: "Social App",
+          category: "Mobile Development"
+        },
+        {
+          title: "Ecommerce Shopping Cart",
+          category: "Web Development"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
