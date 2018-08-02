ee13cadc9771db1951dd148d9552686415c29461
tpham20908 <tpham20908@gmail.com>
2018-08-02 15:19:57 -0400

    api call and parsed to json data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,7 +3,18 @@ import Title from "./components/Title";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "a2cc469ef8f6b9c326aeb4f98c875cdd";
+
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city 
+      + "&appid=" + API_KEY
+    );
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 class Weather extends Component {
-    // a2cc469ef8f6b9c326aeb4f98c875cdd
   render() {
     return (
       <div>
