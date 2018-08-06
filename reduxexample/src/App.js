fe64861cc271db24b36e8ddefc87591a7b8f65c1
tpham20908 <tpham20908@gmail.com>
2018-08-06 16:18:27 -0400

    add post

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from "react-redux";
 import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <Provider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
 
-        <Posts />
-
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-      </div>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
