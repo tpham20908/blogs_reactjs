bcf6fb6eebcad6a5cc7194127544ff3a2b174baf
tpham20908 <tpham20908@gmail.com>
2018-08-11 18:38:41 -0400

    nested link

--- react-router-and-redux/src/App.js
+++ react-router-and-redux/src/App.js
@@ -3,8 +3,35 @@ import { render } from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const Home = () => <h2>Home</h2>;
-const About = () => <h3>About</h3>;
+const About = (props) => {
+  console.log("match", props.match);
+  return <h3>{props.match.params.info}</h3>
+};
 const Users = ({ match }) => <div>{match.url}</div>;
+
+const Info = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={props.match.url + "/phone"}>Phone</Link>
+        </li>
+        <li>
+          <Link to={props.match.url + "/email"}>Email</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Route 
+        path={props.match.url + "/phone"}
+        render={() => <div>Phone: (514) 800-3468</div>}
+      />
+      <Route 
+        path={props.match.url + "/email"}
+        render={() => <div>Email: tpham20908@gmail.com</div>}
+      />
+    </div>
+  )
+}
 
 const App = () => (
   <BrowserRouter>
@@ -14,20 +41,28 @@ const App = () => (
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about/tung">About me</Link>
         </li>
         <li>
           <Link to="/something">Something else</Link>
         </li>
         <li>
-          <Link to="/users">Users</Link>{" "}
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/children">Children link</Link>
+        </li>
+        <li>
+          <Link to="/info">Info</Link>
         </li>
       </ul>
       <hr />
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/something" render={() => <h3>Something else</h3>}/>
+      <Route path="/about/:info" component={About} />
+      <Route path="/something" render={() => <h3>Something else</h3>} />
       <Route path="/users" component={Users} />
+      <Route path="/children" children={() => <h5>This is always rendered</h5>} />
+      <Route path="/info" component={Info} />
     </div>
   </BrowserRouter>
 );
