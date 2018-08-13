4df4e3b2caab5091ad5e530e85107b640e2c2cf4
tpham20908 <tpham20908@gmail.com>
2018-08-12 22:04:17 -0400

    store.subscribe() and unsubscribe()

--- react-router-and-redux/src/App.js
+++ react-router-and-redux/src/App.js
@@ -1,32 +1,51 @@
 import React from "react";
 import { createStore } from "redux";
 
+// constant variables to hold action type
+const ADD_ITEM = "ADD_ITEM";
+const DELETE_ITEM = "DELETE_ITEM";
+
 //action creator
 const addItem = item => {
   return {
-    type: "ADD_ITEM",
+    type: ADD_ITEM,
     item: item
+  };
+};
+const deleteItem = index => {
+  return {
+    type: DELETE_ITEM,
+    index: index
   };
 };
 
 //reducer
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case "ADD_ITEM":
+    case ADD_ITEM:
+      // return new state with added item
       return [...state, action.item];
+    case DELETE_ITEM:
+      // return new state with deleted item (state[action.index])
+      return [...state.slice(0, action.index), ...state.slice(action.index + 1)];
     default:
+      // otherwise, just return the old state
       return state;
   }
 };
 
-var store = createStore(reducer);
+const store = createStore(reducer);
 
-console.log(store.dispatch(addItem("a")));
-console.log(store.getState());
-console.log(store.dispatch(addItem("b")));
-console.log(store.getState());
-console.log(store.dispatch(addItem("c")));
-console.log(store.getState());
+// trigger a console log every time the store updates
+const unsubscribe = store.subscribe(() => console.log (store.getState()));
+store.dispatch(addItem("apple"));
+store.dispatch(addItem("banana"));
+store.dispatch(addItem("cherry"));
+// To unsubscribe, call the method that is returned by store.subscribe()
+// stop subscribing to store updates
+unsubscribe();
+// this will not logged by console
+store.dispatch(addItem("carrot"));
 
 function App() {
   return (
