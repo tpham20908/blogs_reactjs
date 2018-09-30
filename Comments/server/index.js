2fb63b2d9f589a6da7b983d113642f522494c01c
tpham20908 <tpham20908@gmail.com>
2018-09-29 22:09:44 -0400

    router on app

--- Comments/server/index.js
+++ Comments/server/index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const router = require("./router");
 
 const app = express();
 
 // App setup
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3001;
