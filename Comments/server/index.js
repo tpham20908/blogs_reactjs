91085a27dfca9d1e38e9e2e622c51debf58acb4c
tpham20908 <tpham20908@gmail.com>
2018-09-29 21:51:01 -0400

    created server

---
+++ Comments/server/index.js
@@ -0,0 +1,17 @@
+// Main starting points of the application
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+// App setup
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+
+// Server setup
+const port = process.env.PORT || 3001;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server is listening on: " + port);
