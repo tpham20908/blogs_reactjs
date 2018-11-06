e96d2a029961abfe5a96ea17c110af9b07bc8c5c
tpham20908 <tpham20908@gmail.com>
2018-11-06 18:27:28 -0500

    creating user token with jwt-simple

--- comments/server/controllers/authentication.js
+++ comments/server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const { secret } = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, secret);
+}
 
 module.exports.signup = function (req, res, next) {
   const email = req.body.email;
@@ -24,7 +31,7 @@ module.exports.signup = function (req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
       // responde to request indicating the user is created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     })
   })
 }
