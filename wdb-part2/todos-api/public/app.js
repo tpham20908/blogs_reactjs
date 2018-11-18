ae5df640127617799dd3b619198f14702b634f03
tpham20908 <tpham20908@gmail.com>
2018-11-18 10:40:54 -0500

    deleted and updated todo

--- wdb-part2/todos-api/public/app.js
+++ wdb-part2/todos-api/public/app.js
@@ -1,13 +1,50 @@
 $(document).ready(() => {
+  // Retrieve todos
   $.getJSON("/api/todos")
     .then(getTodos);
 
+  // Create todo
   $("#todoInput").keypress(e => {
     if (e.which === 13) {
       createTodo();
     }
   })
+
+  // Delete todo
+  $(".list").on("click", "span", function(e) {
+    e.stopPropagation();
+    removeTodo($(this).parent());
+  })
+
+  // Update todo
+  $(".list").on("click", "li", function() {
+    updateTodo($(this));
+  })
 })
+
+const updateTodo = todo => {
+  const todoId = todo.data("id");
+  const isDone = !todo.data("completed");
+  todo.toggleClass("done");
+  $.ajax({
+    method: "PUT",
+    url: `/api/todos/${todoId}`,
+    data: { completed: isDone }
+  });
+  todo.data("completed", isDone);
+}
+
+const removeTodo = todo => {
+  const todoId = todo.data("id");
+  $.ajax({
+    method: "DELETE",
+    url: `/api/todos/${todoId}`
+  })
+  .then(() => {
+    todo.remove();
+  })
+  .catch(err => console.log(err));
+}
 
 const createTodo = () => {
   var todoInput = $("#todoInput").val();
@@ -26,7 +63,9 @@ const getTodos = (todos) => {
 }
 
 const addTodo = todo => {
-  const newTodo = $("<li class='task'>" + todo.name + "</li>");
+  const newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>");
+  newTodo.data("id", todo._id);
+  newTodo.data("completed", todo.completed);
   if (todo.completed) {
     newTodo.addClass("done");
   }
