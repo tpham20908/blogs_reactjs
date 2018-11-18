8d150b075ff445ea7b1feb31327713dbbc129956
tpham20908 <tpham20908@gmail.com>
2018-11-17 22:29:49 -0500

    created todo

--- wdb-part2/todos-api/public/app.js
+++ wdb-part2/todos-api/public/app.js
@@ -1,14 +1,34 @@
 $(document).ready(() => {
   $.getJSON("/api/todos")
     .then(getTodos);
+
+  $("#todoInput").keypress(e => {
+    if (e.which === 13) {
+      createTodo();
+    }
+  })
 })
 
+const createTodo = () => {
+  var todoInput = $("#todoInput").val();
+  if (todoInput) {
+    $.post("/api/todos", { name: todoInput })
+      .then(newTodo => addTodo(newTodo))
+      .catch(err => console.log(err));
+    $("#todoInput").val("");
+  } else {
+    alert("Todo cannot be empty!");
+  }
+}
+
 const getTodos = (todos) => {
-  todos.map(todo => {
-    const newTodo = $("<li class='task'>" + todo.name + "</li>");
-    if (todo.completed) {
-      newTodo.addClass("done");
-    }
-    $(".list").append(newTodo);
-  })
+  todos.map(todo => addTodo(todo));
+}
+
+const addTodo = todo => {
+  const newTodo = $("<li class='task'>" + todo.name + "</li>");
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $(".list").append(newTodo);
 }
