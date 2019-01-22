29297dbca50b3d21c60d8dc7966a6d7349f496c8
tpham20908 <tpham20908@gmail.com>
2019-01-21 22:19:36 -0500

    rendered product in card

--- store/src/components/Product.js
+++ store/src/components/Product.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
   render() {
-    console.log(this.props.product);
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>{this.props.product.title}</h3>
-      </div>
+      <ProductWrapper className='col-9 col-md-6 col-lg-3 mx-auto my-3'>
+        <div className='card'>
+          <div
+            className='img-container p-5'
+            onClick={() => console.log('img-container clicked')}>
+            <Link to='/details'>
+              <img src={img} alt='product' className='card-img-top' />
+            </Link>
+            <button
+              className='cart-btn'
+              disabled={inCart}
+              onClick={() => console.log('cart-btn clicked')}>
+              Add
+            </button>
+          </div>
+        </div>
+      </ProductWrapper>
     )
   }
 }
+
+const ProductWrapper = styled.div`
+
+`
