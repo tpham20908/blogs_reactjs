8c79b3ca23ca0f09464bf57fe1bc7f16b32782e2
tpham20908 <tpham20908@gmail.com>
2019-04-01 09:27:29 -0400

    combined fetchPosts and fetchUser into fetchPostsAndUsers in action creators

--- blogs_reactjs/src/actions/index.js
+++ blogs_reactjs/src/actions/index.js
@@ -1,23 +1,35 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(userId => dispatch(fetchUser(userId)));
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(userId => dispatch(fetchUser(userId)))
+    .value();
+}
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 }
 
+export const fetchUser = userId => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${userId}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+}
+
+/*
+// this action used to overfetching user
 export const fetchUser = userId => dispatch => _fetchUser(userId, dispatch);
 
 const _fetchUser = _.memoize(async (userId, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${userId}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
 })
-
-/*
-export const fetchUser = userId => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${userId}`);
-  dispatch({ type: 'FETCH_USER', payload: response.data });
-}
 
 export const fetchPosts = () => {
   return async (dispatch, getState) => {
--- blogs_reactjs/src/components/UserHeader.js
+++ blogs_reactjs/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions'
 
 class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -26,4 +21,4 @@ const mapStateToProps = ({ users }, ownProps) => {
   return { user: users.find(user => user.id === ownProps.userId) }
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
